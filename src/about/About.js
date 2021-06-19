8a2bc0803cfd977a3f507680cfdb064d26249c73
Ben Clark <ben@Bens-MacBook-Air.local>
2021-06-19 14:07:10 -0700

    Wrote more content.

--- src/about/About.js
+++ src/about/About.js
@@ -31,6 +31,31 @@ const About = () => {
         hundreds of possible ways to die, so hopefully grouping you all up into
         teams will increase the probability that you keep each other alive.
       </p>
+      <br />
+      <h2 id="afterparty">The After-Party</h2>
+      <p>
+        The race ends at my house which has a pool and a grill, so stick around
+        afterwards for beers, cake, swimming, and the awards ceremony. A dinner
+        of weiner-shaped, grilled foods will be provided with race registration,
+        as well as a keg.{" "}
+        <strong>
+          Because it's a Wednesday night and we have neighbours, the music will
+          stop at 9 PM and you all need to go the fuck home!!!!!!!
+        </strong>{" "}
+        Or I guess some stragglers can stay and we can move things down to the
+        basement and play pool or something... But there isn't room for 20, so
+        hopefully some of you leave.
+      </p>
+      <h2 id="accommodations">Accommodations</h2>
+      <p>
+        There's space on couches here at the house for 3 people. Additionally,
+        part of the back yard is separrated from the pool-area by a fence, and
+        there's room for 3 or 4 tents. If you'd like to crash here the night
+        before or after the event, please enquire when you send your
+        registration email. These will be offered on a first-come-first-served
+        basis. And if we run out of space for you to crash here, you're still
+        invited to go out for breakfast the following morning :)
+      </p>
       <h2 id="sponsors">Sponsors</h2>
       <p>... want to sponsor this event?</p>
     </div>
