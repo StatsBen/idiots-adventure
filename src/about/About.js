0733b39d66705a88ce4bda146bc4a1a290a94fe1
Ben Clark <ben@Bens-MacBook-Air.local>
2021-06-19 13:33:46 -0700

    More styling upgrades, and I wrote a big chunk of the 'about' page.

--- src/about/About.js
+++ src/about/About.js
@@ -2,12 +2,36 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <h2>Host</h2>
+      <h2 id="race">The Race</h2>
       <p>
-        Yep, it's your favourite idiot Ben here once again trying to rope you
-        into doing something stupid with your free time.
+        This is a 5-stage adventure race comprised of 4 separate activities:
       </p>
-      <h2>Sponsors</h2>
+      <ol>
+        <li>A 1.5km river-float on the Illecilewaet,</li>
+        <li>A 2km run with your floaty-toy to the drop-site,</li>
+        <li>An 8km run through the forests of Revelstoke,</li>
+        <li>A rippin' ride down the Pipe Coaster at RMR,</li>
+        <li>An 8km bike ride across the flood-plains of the Columbia.</li>
+      </ol>
+      <br />
+      <h2 id="format">The Format</h2>
+      <p>
+        This will be a <strong>stage race</strong>, meaning the 5 segments will
+        be run like 5 separate little races for which your overall time will be
+        the sum of your times in each of the segments. This will allow the whole
+        gang to group up and laugh at each other periodically throughout the
+        day.
+      </p>
+      <br />
+      <p>
+        Additionally, this will be a <strong>team race</strong>, meaning you
+        will be competing as groups of 2 to 4, and the clock doesn't stop until
+        your whole team crosses the finish-line at each segment. This is
+        primarily for safety, since this is a totally jenky-ass event featuring
+        hundreds of possible ways to die, so hopefully grouping you all up into
+        teams will increase the probability that you keep each other alive.
+      </p>
+      <h2 id="sponsors">Sponsors</h2>
       <p>... want to sponsor this event?</p>
     </div>
   );
